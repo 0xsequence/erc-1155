07c620cb3964b004fdda19a040abbee3a3ac0258
PhABC <ph.castonguay@gmail.com>
2018-07-01 19:15:59 -0400

    Add batchTransfer receive function

--- contracts/token/ERCXXXXTokenReceiver.sol
+++ contracts/token/ERCXXXXTokenReceiver.sol
@@ -6,15 +6,15 @@ pragma solidity ^0.4.24;
 interface ERCXXXXTokenReceiver {
 
   /**
-   * @dev Handle the receipt of a MFT. The ERCXXXX smart contract calls this function on the
-   * recipient after a `transfer`. This function MAY throw to revert and reject the transfer. Return
-   * of other than the magic value MUST result in the transaction being reverted.
+   * @dev Handle the receipt of a fungible token from an MFT contract. The ERCXXXX smart contract calls 
+   * this function on the recipient after a `transfer`. This function MAY throw to revert and reject the 
+   * transfer. Return of other than the magic value MUST result in the transaction being reverted.
    * Returns `bytes4(keccak256("onERCXXXXReceived(address,address,uint256,uint256,bytes)"))` unless throwing.
    * @notice The contract address is always the message sender. A wallet/broker/auction application
    * MUST implement the wallet interface if it will accept safe transfers.
    * @param _operator The address which called `safeTransferFrom` function.
    * @param _from The address from which the token was transfered from.
-   * @param _type The type of token being transfer (where each type is represented as an ID)
+   * @param _type The type of token being transferred (where each type is represented as an ID)
    * @param _amount The amount of token of a given type that was transfered.
    * @param _data Additional data with no specified format.
    */
@@ -27,5 +27,28 @@ interface ERCXXXXTokenReceiver {
     ) 
     external 
     returns(bytes4);
-    
+
+
+  /**
+   * @dev Handle the receipt of multiple fungible tokens from an MFT contract. The ERCXXXX smart contract calls 
+   * this function on the recipient after a `batchTransfer`. This function MAY throw to revert and reject the 
+   * transfer. Return of other than the magic value MUST result in the transaction being reverted.
+   * Returns `bytes4(keccak256("onERCXXXXBatchReceived(address,address,uint256[],uint256[],bytes)"))` unless throwing.
+   * @notice The contract address is always the message sender. A wallet/broker/auction application
+   * MUST implement the wallet interface if it will accept safe transfers.
+   * @param _operator The address which called `safeTransferFrom` function.
+   * @param _from The address from which the token was transfered from.
+   * @param _types Array of types of token being transferred (where each type is represented as an ID)
+   * @param _amounts Array of amount of object per type to be transferred.
+   * @param _data Additional data with no specified format.
+   */
+  function onERCXXXXBatchReceived(
+    address _operator,
+    address _from,
+    uint256[] _types,
+    uint256[] _amounts,
+    bytes _data
+    )
+    external
+    returns(bytes4)
 }
