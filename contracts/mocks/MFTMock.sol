22cfc633dddb5bb40257fca2729130030e096df0
PhABC <ph.castonguay@gmail.com>
2018-07-02 00:58:56 -0400

    Remove useless section

--- contracts/mocks/MFTMock.sol
+++ contracts/mocks/MFTMock.sol
@@ -4,14 +4,9 @@ import '../token/MFT.sol';
 
 contract MFTMock is MFT {
 
-  uint256 initBalance = 0x1000100010001000100010001000100010001000100010001000100010001 * 256;
-  // uint256 initBalance = 0x1010101010101010101010101010101010101010101010101010101010101 * 128;
+  constructor() public { }
 
-  constructor() public {
-
-  }
-
-  function mint(address _address, uint256 _type, uint256 _value) public {
+  function mockMint(address _address, uint256 _type, uint256 _value) public {
     _updateTypeBalance(_address, _type, _value, Operations.Add);
   }
 
