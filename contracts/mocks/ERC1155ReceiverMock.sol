de2d0af8852a588c1eb7160b1a5b742d5fa8ce90
Alexander Atallah <alexander.atallah@gmail.com>
2019-10-16 11:55:00 -0700

    add back interfaces for public methods

--- contracts/mocks/ERC1155ReceiverMock.sol
+++ contracts/mocks/ERC1155ReceiverMock.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.12;
 
-import '../tokens/ERC1155/ERC1155.sol';
+import '../interfaces/IERC1155.sol';
 
 
 // Contract to test safe transfer behavior.
@@ -45,8 +45,8 @@ contract ERC1155ReceiverMock {
     // To check the following conditions;
     //   All the balances in the transfer MUST have been updated to match the senders intent before any hook is called on a recipient.
     //   All the transfer events for the transfer MUST have been emitted to reflect the balance changes before any hook is called on a recipient.
-    uint256 fromBalance = ERC1155(msg.sender).balanceOf(_from, _id);
-    uint256 toBalance = ERC1155(msg.sender).balanceOf(address(this), _id);
+    uint256 fromBalance = IERC1155(msg.sender).balanceOf(_from, _id);
+    uint256 toBalance = IERC1155(msg.sender).balanceOf(address(this), _id);
     emit TransferSingleReceiver(_from, address(this), fromBalance, toBalance);
 
     if (shouldReject == true) {
@@ -81,8 +81,8 @@ contract ERC1155ReceiverMock {
       fromAddressArray[i] = _from;
       toAddressArray[i] = (address(this));
     }
-    uint256[] memory fromBalances = ERC1155(msg.sender).balanceOfBatch(fromAddressArray, _ids);
-    uint256[] memory toBalances = ERC1155(msg.sender).balanceOfBatch(toAddressArray, _ids);
+    uint256[] memory fromBalances = IERC1155(msg.sender).balanceOfBatch(fromAddressArray, _ids);
+    uint256[] memory toBalances = IERC1155(msg.sender).balanceOfBatch(toAddressArray, _ids);
     emit TransferBatchReceiver(_from, address(this), fromBalances, toBalances);
 
     if (shouldReject == true) {
@@ -99,7 +99,7 @@ contract ERC1155ReceiverMock {
    *      This function MUST NOT consume more than 5,000 gas.
    * @return Wheter ERC-165 or ERC1155TokenReceiver interfaces are supported.
    */
-  function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
+  function supportsInterface(bytes4 interfaceID) external view returns (bool) {
     return  interfaceID == 0x01ffc9a7 || // ERC-165 support (i.e. `bytes4(keccak256('supportsInterface(bytes4)'))`).
       interfaceID == 0x4e2312e0;         // ERC-1155 `ERC1155TokenReceiver` support (i.e. `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`).
   }
