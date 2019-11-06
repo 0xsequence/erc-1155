9cf76fe2e2596eac96ca42f4371c49ff5ce66e9c
PhABC <ph.castonguay@gmail.com>
2019-11-06 15:32:19 -0500

    add mint/burn interface

---
+++ contracts/interfaces/IERC1155MintBurn.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.12;
+
+/**
+ * Recommended inferface for public facing minting and burning functions.
+ * These public methods should have restricted access.
+ */
+interface IERC1155MintBurn {
+
+  /***************************************|
+  |        Public Minting Functions       |
+  |______________________________________*/
+
+  /**
+   * @dev Mint _amount of tokens of a given id if not frozen and if max supply not exceeded
+   * @param _to     The address to mint tokens to.
+   * @param _id     Token id to mint
+   * @param _amount The amount to be minted
+   * @param _data   Byte array of data to pass to recipient if it's a contract
+   */
+  function mint(address _to, uint256 _id, uint256 _amount, bytes calldata _data) external;
+
+  /**
+   * @dev Mint tokens for each ids in _ids
+   * @param _to      The address to mint tokens to.
+   * @param _ids     Array of ids to mint
+   * @param _amounts Array of amount of tokens to mint per id
+   * @param _data    Byte array of data to pass to recipient if it's a contract
+   */
+  function batchMint(address _to, uint256[] calldata _ids, uint256[] calldata _amounts, bytes calldata _data) external;
+
+
+  /***************************************|
+  |        Public Minting Functions       |
+  |______________________________________*/
+
+  /**
+   * @notice Burn _amount of tokens of a given token id
+   * @param _from    The address to burn tokens from
+   * @param _id      Token id to burn
+   * @param _amount  The amount to be burned
+   */
+  function burn(address _from, uint256 _id, uint256 _amount) external;
+
+  /**
+   * @notice Burn tokens of given token id for each (_ids[i], _amounts[i]) pair
+   * @param _from     The address to burn tokens from
+   * @param _ids      Array of token ids to burn
+   * @param _amounts  Array of the amount to be burned
+   */
+  function batchBurn(address _from, uint256[] calldata _ids, uint256[] calldata _amounts) external;
+
+}
+
