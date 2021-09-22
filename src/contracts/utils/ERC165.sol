bf849c3f1c2c6a480c17743ecffef0e8ddce552d
Philippe Castonguay <ph.castonguay@gmail.com>
2021-09-22 19:19:32 -0400

    Update ERC-165 interface (#78)
    * Update ERC-165 interface
    
    * add typechain changes

--- src/contracts/utils/ERC165.sol
+++ src/contracts/utils/ERC165.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.7.4;
+import "../interfaces/IERC165.sol";
 
-abstract contract ERC165 {
+abstract contract ERC165 is IERC165 {
   /**
    * @notice Query if a contract implements an interface
    * @param _interfaceID The interface identifier, as specified in ERC-165
    * @return `true` if the contract implements `_interfaceID`
    */
-  function supportsInterface(bytes4 _interfaceID) virtual public pure returns (bool) {
+  function supportsInterface(bytes4 _interfaceID) virtual override public pure returns (bool) {
     return _interfaceID == this.supportsInterface.selector;
   }
 }
